ddb048eb2bc938d516efb13d98e99e31d5dad51c
abemaprog <appdev.abe@gmail.com>
2024-10-08 23:02:40 +0900

    Initial Commit

---
+++ DendaiApp/DendaiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DendaiAppApp.swift
+//  DendaiApp
+//
+//  Created by Manato Abe on 2024/10/08.
+//
+
+import SwiftUI
+
+@main
+struct DendaiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
