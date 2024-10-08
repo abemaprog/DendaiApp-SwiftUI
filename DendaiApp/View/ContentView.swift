c570a39764628fac02a1c02f5e752ddec04dc501
abemaprog <appdev.abe@gmail.com>
2024-10-08 23:42:14 +0900

    TabViewの配置

--- DendaiApp/View/ContentView.swift
+++ DendaiApp/View/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
