1699548b78de9c69b53f4776d6e67808cfcaa83c
abemaprog <appdev.abe@gmail.com>
2024-10-12 12:30:46 +0900

    機能要望（Linkの追加）

--- DendaiApp/ViewModel/LinkViewModel.swift
+++ DendaiApp/ViewModel/LinkViewModel.swift
@@ -22,7 +22,11 @@ class LinkViewModel: ObservableObject {
             //　総合メディアセンター
             LinkItem(title: "総合メディアセンター", image: "book", url: "https://www.mrcl.dendai.ac.jp/mrcl/#gsc.tab=0", color: .purple),
             // 教科書購入サイト
-            LinkItem(title: "教科書販売", image: "books.vertical", url: "https://tdu-coop.jp/", color: .yellow)
+            LinkItem(title: "教科書販売", image: "books.vertical", url: "https://tdu-coop.jp/", color: .yellow),
+            //　科学技術概論
+            LinkItem(title: "科学技術概論ポータルサイト", image: "gear", url: "https://tdu-sc.notion.site/6237e3b95b3f4090aa0ee3f57617e028", color: .gray),
+            // Physics-e
+            LinkItem(title: "Physics-e", image: "waveform.path.ecg", url: "https://ptg01.physics-e.com/DENDAI/Physicse/users/", color: .indigo)
         ]
         
         otherLinkItems = [
