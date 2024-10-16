c42a5bcc22548480fc2c714d38a9e2821dd62968
abemaprog <appdev.abe@gmail.com>
2024-10-16 12:14:01 +0900

    アプリ名の変更、アイコンの変更

---
+++ DendaiApp/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  DendaiApp
+//
+//  Created by Manato Abe on 2024/10/16.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
